e57df4147d7d023fe2981189a21b59b435af947b
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2023-11-16 14:51:31 -0600

    Proyecto base

---
+++ NetApp/NetApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NetApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NetApp/NetApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NetApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
