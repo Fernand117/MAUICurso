333c05d0cbd49c06e93b10d7e82e4c243388b6d4
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2023-11-21 20:40:30 -0600

    .

---
+++ XamRideDroid/XamRideDroid/Views/LoginActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+
+namespace XamRideDroid.Views
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class LoginActivity : AppCompatActivity
+    {
+        private EditText txtUsuario;
+        private EditText txtPassword;
+        private Button   btnLogin;
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.login_activity);
+            SupportActionBar.Hide();
+
+            txtUsuario  = (EditText)FindViewById(Resource.Id.txtUsuario);
+            txtPassword = (EditText)FindViewById(Resource.Id.txtPassword);
+            btnLogin = (Button)FindViewById(Resource.Id.btnValidateForm);
+            
+            btnLogin.Click += BtnLoginOnClick;
+        }
+
+        private void BtnLoginOnClick(object sender, EventArgs e)
+        {
+            Console.WriteLine("Dentro del botón del login");
+        }
+    }
+}
